754e97e229ddcb179c3346d6e693e1f88f4c679f
Fleta <yasoueisin@gmail.com>
2019-11-05 21:57:35 +0900

    add static files and requirements

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from starlette.routing import Mount
+from starlette.staticfiles import StaticFiles
+from starlette.responses import HTMLResponse
+from os import path
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def read_root():
+    with open("./static/index.html", "r") as html_file:
+        return HTMLResponse(html_file.read(), status_code=200)
+    # return HTMLResponse(open(path.abspath("./static/index.html")), status_code=200)
