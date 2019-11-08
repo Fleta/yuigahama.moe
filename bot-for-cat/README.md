b6b97ba8ea52e0edc0e14d9d560b1f4aa196186b
Fleta <yasoueisin@gmail.com>
2019-11-08 23:21:42 +0900

    add sample response api

--- bot-for-cat/README.md
+++
@@ -1,3 +0,0 @@
-# BOT-FOR-CAT
-
-한국의 길고양이들을 위한 정보를 제공하는 봇의 API 서버입니다. [Code for Cat](https://code4cat.org) 에서 아이디어를 받았습니다.
--- main.py
+++ main.py
@@ -2,7 +2,19 @@ from fastapi import FastAPI
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
-from os import path
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+from starlette.responses import JSONResponse
+
+from os.path import abspath, dirname
+import sys
+
+sys.path.append(abspath(dirname(__file__) + "\\..\\"))
+import bot_for_cat
+
+class ResponseModel(BaseModel):
+    status: str
+    response: str = None
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory="static"), name="static")
@@ -16,3 +28,11 @@ async def read_root():
 async def resume_page():
     with open("./static/resume.html", "r", encoding="UTF8") as resume_file:
         return HTMLResponse(resume_file.read(), status_code=200)
+
+@app.get("/api/catbot/test")
+async def test_response():
+    response_data = {
+        'status': 200,
+        'response': bot_for_cat.response.say_hello()
+    }
+    return JSONResponse(content=jsonable_encoder(ResponseModel(**response_data)))
