1f54f7629ac542c2fc2095b5d9643ec8bb9bdaca
Fleta <yasoueisin@gmail.com>
2019-11-09 03:22:26 +0900

    add: test receive function and logger

--- main.py
+++ main.py
@@ -7,13 +7,28 @@ from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse
 
 from os.path import abspath, dirname
-import sys
+import sys, logging
 
 sys.path.append(abspath(dirname(__file__) + "\\..\\"))
-import bot_for_cat
+from bot_for_cat import response
+from bot_for_cat.models import normal_response, kakao_request
 
 app = FastAPI()
 app.mount('/static', StaticFiles(directory="static"), name="static")
+
+logger = logging.getLogger("app")
+logger.setLevel(logging.DEBUG)
+
+# fh = TimedRotatingFileHandler(filename = "./logs/yuigahama.moe.log", when="midnight", interval=1, encoding="UTF-8")
+# fh.setLevel(logging.ERROR)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%ㅣ(levelname)s|%(filename)s:%(lineno)s] %(asctime)s >> %(message)s')
+# fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+# logger.addHandler(fh)
+logger.addHandler(ch)
 
 @app.get("/")
 async def read_root():
@@ -29,8 +44,19 @@ async def resume_page():
 async def test_response():
     response_data = {
         'status': 200,
-        'response': bot_for_cat.response.say_hello()
+        'response': response.say_hello()
     }
     return JSONResponse(content=jsonable_encoder(
-        bot_for_cat.models.normal_response.Response(**response_data)) 
+        normal_response.Response(**response_data)) 
+    )
+
+@app.post("/api/catbot/receive")
+async def handle_receive_event(message: dict):
+    logger.debug(jsonable_encoder(kakao_request.Request(**message)))
+    response_data = {
+        'status': 200,
+        'response': response.say_hello()
+    }
+    return JSONResponse(content=jsonable_encoder(
+        normal_response.Response(**response_data)) 
     )
