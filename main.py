33a7e1ba4ac404e8d1862ceade436e4ac3f6a733
Fleta <yasoueisin@gmail.com>
2019-11-13 01:29:22 +0900

    fix: PEP8 convention

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from os.path import abspath, dirname
+import sys, logging
+
 from fastapi import FastAPI
 from starlette.routing import Mount
 from starlette.staticfiles import StaticFiles
@@ -5,9 +8,6 @@ from starlette.responses import HTMLResponse
 from pydantic import BaseModel
 from fastapi.encoders import jsonable_encoder
 from starlette.responses import JSONResponse, FileResponse
-
-from os.path import abspath, dirname
-import sys, logging
 
 sys.path.append(abspath(dirname(__file__) + "\\..\\"))
 from bot_for_cat import response
@@ -30,15 +30,18 @@ ch.setFormatter(formatter)
 # logger.addHandler(fh)
 logger.addHandler(ch)
 
+
 @app.get("/")
 async def read_root():
     with open("./static/index.html", "r") as html_file:
         return HTMLResponse(html_file.read(), status_code=200)
 
+
 @app.get("/resume")
 async def resume_page():
     with open("./static/resume.html", "r", encoding="UTF8") as resume_file:
         return HTMLResponse(resume_file.read(), status_code=200)
+
 
 @app.get("/api/catbot/test")
 async def test_response():
@@ -47,8 +50,9 @@ async def test_response():
         'response': response.say_hello()
     }
     return JSONResponse(content=jsonable_encoder(
-        normal_response.Response(**response_data)) 
-    )
+        normal_response.Response(**response_data)
+    ))
+
 
 @app.get("/api/catbot/cat-image/{item_id}")
 async def serve_cat_image(item_id):
@@ -67,9 +71,11 @@ async def simple_text_response(message: dict):
         # kakao_response.Reponse(**response_data)
     )
 
+
 @app.post("/api/catbot/simple-image-response")
 async def simple_image_response(message: dict):
     return response.serve_sample_image("https://placekitten.com/200/300", "sample cat image")
+
 
 @app.post("/api/catbot/call-cat")
 async def call_cat_response(message: dict):
